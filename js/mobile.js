ba2780412980650ce51cf03131962f4acd20678d
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2025-12-15 17:01:14 +0800

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "雅姐音乐";
+        dom.mobileToolbarTitle.textContent = "<span style='color: #ff9800;'>李小雅 - 音乐</span><br><span style='font-size: 12px; color: #00bcd4;'>来自一位顾不上爱你的人制作!</span>";
     }
 
     function updateMobileOverlayScrim() {
